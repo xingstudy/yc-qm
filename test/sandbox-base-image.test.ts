6e491bddf7f32895e7a6b494bb53068b99e2066d
xingstudy <17332086+xingstudy@users.noreply.github.com>
2026-09-02 17:44:35 +0800

    test: track gh 2.99.0 pin in sandbox base image test

--- test/sandbox-base-image.test.ts
+++ test/sandbox-base-image.test.ts
@@ -29,7 +29,7 @@ test("the sandbox base replaces npm's vulnerable bundled tar", () => {
 
 test("the sandbox base builds a patched GitHub CLI", () => {
   const dockerfile = readFileSync(new URL("../fly/Dockerfile", import.meta.url), "utf8");
-  assert.match(dockerfile, /ARG GH_VERSION=2\.98\.0/);
+  assert.match(dockerfile, /ARG GH_VERSION=2\.99\.0/);
   assert.match(dockerfile, /ARG X_MOD_VERSION=0\.40\.0/);
   assert.match(dockerfile, /go get "golang\.org\/x\/mod@v\$\{X_MOD_VERSION\}"/);
   assert.match(dockerfile, /go version -m \/usr\/local\/bin\/gh \| grep -Eq/);
